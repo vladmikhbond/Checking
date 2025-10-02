703e3ca16eda37079307ba8fcd07e160e5f6374f
vladmikhbond <vladmikhbond@gmale.com>
2025-10-02 17:22:56 +0000

    .

--- app/dal.py
+++ app/dal.py
@@ -1,11 +1,11 @@
 from datetime import datetime
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session
-from .models.pss_models import Ticket
+from .models.models import Base, Ticket
 
 # Створюємо engine (SQLite файл лежить у /data/PSS.db)
 engine = create_engine(
-    "sqlite:////data/PSS.db",
+    "sqlite:////data/Checking.db",
     echo=True,
     connect_args={"check_same_thread": False}  # потрібно для SQLite + багатопоточного доступу
 )
@@ -20,3 +20,7 @@ def get_db():
         yield db
     finally:
         db.close()
+
+if __name__ == "__main__":
+    Base.metadata.create_all(engine)
+
--- app/models/models.py
+++ app/models/models.py
@@ -1,5 +1,6 @@
 """ All models for PSS.db """
 from datetime import datetime, timedelta
+from typing import List
 from sqlalchemy import ForeignKey, String, DateTime, Integer, Text, LargeBinary
 from sqlalchemy.orm import Mapped, mapped_column, relationship, DeclarativeBase
 
@@ -10,7 +11,55 @@ class Base(DeclarativeBase):
 class Test(Base):
     __tablename__ = "tests"
 
-    id: Mapped[str] = mapped_column(primary_key=True)  # назва
-    tutor_id: Mapped[str] = mapped_column(String)
+    title: Mapped[str] = mapped_column(String, primary_key=True)  # назва
+    username: Mapped[str] = mapped_column(String)
     body: Mapped[str] = mapped_column(String)
+    # nav
+    questions: Mapped[List["Question"]] = relationship(back_populates="test")
+    seances: Mapped[List["Seance"]] = relationship(back_populates="test")
+    
+class Question(Base):
+    __tablename__ = "questions"
+
+    test_title: Mapped[str] = mapped_column(ForeignKey("tests.title", ondelete="CASCADE"), primary_key=True)  # назва
+    number: Mapped[int] = mapped_column(Integer, primary_key=True)
+    topic: Mapped[str] = mapped_column(String)
+    kind: Mapped[str] = mapped_column(String)              # '!', '#' 
+    text: Mapped[str] = mapped_column(Text)
+    answers: Mapped[str] = mapped_column(Text) 
+    # nav           
+    test: Mapped["Test"] = relationship(back_populates="questions")
+
+class Seance(Base):
+    __tablename__ = "sessions"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True) 
+    test_title: Mapped[str] = mapped_column(String, ForeignKey("tests.title", ondelete="CASCADE"),)
+    tutor_name: Mapped[str] = mapped_column(String)
+    open_time: Mapped[datetime] = mapped_column(DateTime, default=None)
+    open_minutes: Mapped[int] = mapped_column(Integer, default=0)
+    stud_filter: Mapped[str] = mapped_column(String, default='')
+    # nav
+    test: Mapped["Test"] = relationship(back_populates="seances")
+
+
+class Ticket(Base):
+    __tablename__ = "tickets"
+ 
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True) 
+    session_id: Mapped[int] = mapped_column(Integer) 
+    username: Mapped[str] = mapped_column(String)
+    test_title: Mapped[str] = mapped_column(String)
+    session_close_time: Mapped[datetime] = mapped_column(DateTime, default=None)
+    questions_number: Mapped[int] = mapped_column(Integer)
+    next_question_number: Mapped[int] = mapped_column(Integer) 
+    protocol: Mapped[str] = mapped_column(Text) 
+
+
+    #  nav
+
+    session: Mapped["Test"] = relationship(back_populates="tickets")
+
+    
+
     
